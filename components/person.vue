3db9aac3dac9e529586ee2230cf38158a6a0785a
Serge R <747500@gmail.com>
2021-01-17 00:33:16 +0300

    nav, person

---
+++ components/person.vue
@@ -0,0 +1,37 @@
+
+
+<template id="person">
+	<div>
+		<div>Person list</div>
+		<ul>
+			<li
+				v-for="p in person"
+				v-bind:class="{ 'strike': !p.status }"
+				v-bind:key="p.id">
+				<div>{{ p.lastName }}</div>
+				<div>{{ p.firstName }}</div>
+			</li>
+		</ul>
+
+	</div>
+</template>
+
+<script>
+
+export default {
+	template: 'person',
+	data: function () {
+		return  {
+			person: [
+				{
+					id: 1,
+					firstName: 'Иван',
+					lastName: 'Петров',
+					status: true
+				}
+			]
+		}
+	},
+};
+
+</script>
--- main.js
+++ main.js
@@ -10,6 +10,7 @@ import l2 from './components/l2.vue';
 import l3 from './components/l3.vue';
 import l4 from './components/l4.vue';
 import l9 from './components/l9.vue';
+import person from './components/person.vue';
 
 Vue.component('raw-view', rawView);
 
@@ -20,11 +21,38 @@ new Vue({
 		l2: l2,
 		l3: l3,
 		l4: l4,
-		l9: l9
+		l9: l9,
+		person: person
 	},
 	data: {
 		currentView: 'l1',
-		messages: []
+		messages: [],
+		nav: [
+			{
+				id: 'l1',
+				title: 'Lesson 1'
+			},
+			{
+				id: 'l2',
+				title: 'Lesson 2'
+			},
+			{
+				id: 'l3',
+				title: 'Lesson 3'
+			},
+			{
+				id: 'l4',
+				title: 'Lesson 4'
+			},
+			{
+				id: 'l9',
+				title: 'Часть 9 - Создание событий'
+			},
+			{
+				id: 'person',
+				title: 'Person'
+			}
+		]
 	},
 	methods: {
 		switchView: function (view) {
