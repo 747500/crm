84900dc2fe17ba36edb6c0bc3f46838f0a37284a
Serge R <747500@gmail.com>
2021-01-21 01:37:16 +0300

    selects VueFormulate as "usable component"

--- components/person_edit.vue
+++ components/person_edit.vue
@@ -2,13 +2,11 @@
 
 <template>
 	<div class="panel-body">
-		<vue-form-generator
-			:schema="schema"
-			:model="model"
-			:options="formOptions">
-		</vue-form-generator>
 
-		<hr>
+		<FormulateForm
+			v-model="model"
+			:schema="person"
+			/>
 
 	</div>
 </template>
@@ -16,16 +14,12 @@
 <script>
 
 import Vue from 'vue'
-import VueFormGenerator from 'vue-form-generator'
-//import schema from './person_edit_schema.json'
 
-//import 'vue-form-generator/dist/vfg.css'
-
-Vue.use(VueFormGenerator)
+import VueFormulate from '@braid/vue-formulate'
+Vue.use(VueFormulate)
 
 export default {
 	components: {
-//		VueFormGenerator: VueFormGenerator
 	},
 	data: function () {
 	    return {
@@ -37,81 +31,37 @@ export default {
 				password: 'J0hnD03!x4',
 				skills: ['Javascript', 'VueJS'],
 				email: 'john.doe@gmail.com',
-				status: true
+				status: true,
+				comment: '',
+				b: ''
 			},
-			schema2: schema2,
-			schema: {
-				fields: [
-					{
-						type: 'input',
-						inputType: 'text',
-						label: 'ID (disabled text field)',
-						model: 'id',
-						readonly: true,
-						disabled: true
-					},
-					{
-						type: 'input',
-						inputType: 'text',
-						label: 'Фамилия',
-						model: 'lastName',
-						placeholder: '',
-						featured: true,
-						required: true
-					},
-					{
-						type: 'input',
-						inputType: 'text',
-						label: 'Имя',
-						model: 'firstName',
-						placeholder: '',
-						featured: true,
-						required: true
-					},
-					{
-						type: 'input',
-						inputType: 'text',
-						label: 'Отчество',
-						model: 'middleName',
-						placeholder: '',
-						featured: true,
-						required: true
-					},
-					{
-						type: 'input',
-						inputType: 'password',
-						label: 'Password',
-						model: 'password',
-						min: 6,
-						required: true,
-						hint: 'Minimum 6 characters',
-						validator: VueFormGenerator.validators.string
-					},
-					{
-						type: 'select',
-						label: 'Skills',
-						model: 'skills',
-						values: ['Javascript', 'VueJS', 'CSS3', 'HTML5']
-					},
-					{
-						type: 'input',
-						inputType: 'email',
-						label: 'E-mail',
-						model: 'email',
-						placeholder: 'User\'s e-mail address'
-					},
-					{
-						type: 'checkbox',
-						label: 'Status',
-						model: 'status',
-						default: true
-					}				]
-			},
-			formOptions: {
-				validateAfterLoad: true,
-				validateAfterChanged: true,
-				validateAsync: true
-			}
+			person: [
+				{
+					name: 'lastName',
+					type: 'text',
+					label: 'Фамилия'
+				},
+				{
+					name: 'firstName',
+					type: 'text',
+					label: 'Имя'
+				},
+				{
+					name: 'middleName',
+					type: 'text',
+					label: 'Отчество'
+				},
+				{
+					name: 'b',
+					type: 'date',
+					label: 'Отчество'
+				},
+				{
+					name: 'comment',
+					type: 'textarea',
+					label: 'Комментарии'
+				}
+			]
 		}
 	},
 	methods: {
