b96a116955327769c92d9f37ebb603e19e290e12
Serge R <747500@gmail.com>
2021-01-22 04:13:02 +0300

    moves router stuff to sep. file

--- main.js
+++ main.js
@@ -11,64 +11,7 @@ Vue.component('raw-view', rawView)
 
 // --------------------------------------------------------------------------
 
-import VueRouter from 'vue-router'
-Vue.use(VueRouter)
-
-import l1 from './components/l1.vue'
-import l2 from './components/l2.vue'
-import l3 from './components/l3.vue'
-import l4 from './components/l4.vue'
-import l9 from './components/l9.vue'
-import person from './components/person.vue'
-import person_edit from './components/person_edit.vue'
-
-const routes = [
-	{
-		title: 'Lesson 1',
-		name: 'l1',
-		path: '/l1',
-		component: l1
-	},
-	{
-		title: 'Lesson 2',
-		name: 'l2',
-		path: '/l2',
-		component: l2
-	},
-	{
-		title: 'Lesson 3',
-		name: 'l3',
-		path: '/l3',
-		component: l3
-	},
-	{
-		title: 'Lesson 4',
-		name: 'l4',
-		path: '/l4',
-		component: l4
-	},
-	{
-		title: 'Часть 9 - Создание событий',
-		name: 'l9',
-		path: '/l9',
-		component: l9
-	},
-	{
-		title: 'Люди',
-		name: 'person',
-		path: '/person',
-		component: person,
-		children: [
-			{
-				name: 'person_edit',
-				path: ':id',
-				component: person_edit,
-			}
-		]
-	}
-]
-
-const router = new VueRouter({ routes })
+import router from './router.js'
 
 // --------------------------------------------------------------------------
 
@@ -95,8 +38,7 @@ new Vue({
 	store,
 	data: {
 		currentView: 'l1',
-		messages: [],
-		routes
+		messages: []
 	},
 	methods: {
 		haveMessage (message) {
---
+++ router.js
@@ -0,0 +1,63 @@
+'use strict'
+
+import Vue from 'vue'
+
+import VueRouter from 'vue-router'
+
+import l1 from './components/l1.vue'
+import l2 from './components/l2.vue'
+import l3 from './components/l3.vue'
+import l4 from './components/l4.vue'
+import l9 from './components/l9.vue'
+import person from './components/person.vue'
+import person_edit from './components/person_edit.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+	{
+		title: 'Lesson 1',
+		name: 'l1',
+		path: '/l1',
+		component: l1
+	},
+	{
+		title: 'Lesson 2',
+		name: 'l2',
+		path: '/l2',
+		component: l2
+	},
+	{
+		title: 'Lesson 3',
+		name: 'l3',
+		path: '/l3',
+		component: l3
+	},
+	{
+		title: 'Lesson 4',
+		name: 'l4',
+		path: '/l4',
+		component: l4
+	},
+	{
+		title: 'Часть 9 - Создание событий',
+		name: 'l9',
+		path: '/l9',
+		component: l9
+	},
+	{
+		title: 'Люди',
+		name: 'person',
+		path: '/person',
+		component: person,
+		children: [
+			{
+				name: 'person_edit',
+				path: ':id',
+				component: person_edit,
+			}
+		]
+	}
+]
+
+export default new VueRouter({ routes })
