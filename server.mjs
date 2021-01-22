1d4b62cc71466604a7811348f187048bebf1755f
Serge R <747500@gmail.com>
2021-01-22 16:17:28 +0300

    adds initital web server

---
+++ server.mjs
@@ -0,0 +1,18 @@
+'use strict'
+
+import express from 'express'
+
+const app = express()
+const port = 3000
+
+/*
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+*/
+
+app.use(express.static('public'));
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
