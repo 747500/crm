e82b0a3e6fda35317bd24a5f07ba4a023de6e71e
Serge R <747500@gmail.com>
2021-01-22 23:23:05 +0300

    adds logging (morgan), body-parser, get/post/put methods for person

--- server.mjs
+++ server.mjs
@@ -1,15 +1,41 @@
 'use strict'
 
 import express from 'express'
+import morgan from 'morgan'
+//import bodyParser from 'body-parser'
 
 const app = express()
 const port = 3000
+
+app.use(morgan('combined'))
+app.use(express.json())
 
 /*
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 */
+
+app.get('/person/:id', (req, res) => {
+	res.send({
+		personId: '1',
+		lastName: 'Иванов',
+		firstName: 'Иван',
+		middleName: 'Иванович',
+		birthDay: '2001-02-03',
+		passport: 'имеется'
+	})
+})
+
+app.post('/person', (req, res) => {
+	console.log('POST', req.body);
+	res.send({ status: 'ok' })
+})
+
+app.put('/person', (req, res) => {
+	console.log('PUT', req.body);
+	res.send({ status: 'ok' })
+})
 
 app.use(express.static('public'));
 
