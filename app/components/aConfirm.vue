541fcc0eea1a07576de96d33c398cfe1d6d15787
Serge R <747500@gmail.com>
2021-02-04 01:54:45 +0300

    adds aConfirm template

---
+++ app/components/aConfirm.vue
@@ -0,0 +1,44 @@
+<template>
+	<span>
+		<a v-if="confirm" href="" @click.prevent="onCancel">{{ $props.message || 'Cancel' }}</a>
+		<a href="" @click.prevent="onClick"><slot></slot></a>
+	</span>
+</template>
+
+<script>
+
+export default {
+	name: 'aYesNo',
+	props: {
+		onconfirm: {
+			type: Function,
+			required: true
+		},
+		message: {
+			type: String
+			required: false
+		}
+	},
+	data () {
+		return {
+			confirm: null
+		}
+	},
+	methods: {
+		onClick (event) {
+			if (this.confirm) {
+				this.confirm(event)
+				this.confirm = null
+			}
+			else
+			{
+				this.confirm = this.$props.onconfirm
+			}
+		},
+		onCancel (event) {
+			this.confirm = null
+		}
+	}
+}
+
+</script>
