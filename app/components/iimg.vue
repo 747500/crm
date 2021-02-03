57b11f455885b5b3c4f819f31c75f31cc9c7f0bf
Serge R <747500@gmail.com>
2021-02-03 08:06:45 +0300

    adds lost revokeObjectURL

--- app/components/iimg.vue
+++ app/components/iimg.vue
@@ -1,8 +1,15 @@
 <script>
 
-
 	export default {
 		name: 'iimg',
+
+		props: {
+			src: {
+				type: [ Blob, File ], // String works too
+				required: false
+			}
+		},
+
 		render(createElement) {
 
 			//console.log('<<< iimg src=', typeof this.$props.src)
@@ -17,21 +24,20 @@
 			this.$nextTick(() => {
 				try {
 					if (this.$props.src) {
-						img.elm.src = URL.createObjectURL(this.$props.src);
+						img.elm.src = URL.createObjectURL(this.$props.src)
+						img.elm.onload = event => {
+							// no longer need to read the blob so it revoked
+							URL.revokeObjectURL(this.$props.src)
+						}
 					}
+
 				}
 				catch (e) {
-					console.error('>>>>>', e)
+					console.error('iimg.vue', e)
 				}
 			});
 
 			return img;
-		},
-		props: {
-			src: {
-				type: [ Blob, File ],
-				required: false,
-			}
 		}
 	}
 
