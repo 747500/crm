2dccec897517e6207f0275aa02a20ebe4ef275fd
Serge R <747500@gmail.com>
2021-02-18 15:09:32 +0300

    changes style name to less popular one )

--- app/components/List.vue
+++ app/components/List.vue
@@ -1,6 +1,6 @@
 <template>
 
-	<ul>
+	<ul class="doc-list">
 		<li v-if="$listeners.create" class="create">
 			<div class="btn-group btn-group-sm" role="group" aria-label="">
 				<button @click.prevent="() => $emit('create')" class="btn btn-primary">
@@ -83,37 +83,35 @@ export default {
 
 <style>
 
-ul.list {
-	margin-top: 0;
-	margin-bottom: 0;
-	padding-top: 0.33em;
-	padding-bottom: 0.33em;
+ul.doc-list {
+	margin: 0;
+	padding: 0.33em 0;
 }
 
-ul.list li {
+ul.doc-list li {
 	padding: .33em;
 	display: flex;
 	border-bottom: 1px solid var(--border-color);
 }
 
-ul.list li:hover {
+ul.doc-list li:hover {
 	background-color: var(--hover-color);
 }
 
-ul.list li .item {
+ul.doc-list li .item {
 	flex: 10;
 }
 
-ul.list li .item.active {
+ul.doc-list li .item.active {
 	cursor: pointer;
 }
 
-ul.list li .toolbar {
+ul.doc-list li .toolbar {
 	flex: 1;
 	text-align: right;
 }
 
-ul.list li.create {
+ul.doc-list li.create {
 	list-style-type: circle;
 }
 
