950c247d724314010acd687b8a9d7f24ce46ab17
Serge R <747500@gmail.com>
2021-03-03 04:10:22 +0300

    split server up to modules

--- server/server.mjs
+++ server/server.mjs
@@ -11,7 +11,6 @@ import mongoose from 'mongoose'
 
 import models from './db_schema.mjs'
 
-<<<<<<< HEAD
 import ServicesRun from './services/index.mjs'
 
 import CONFIG from './config.js'
@@ -26,88 +25,6 @@ ServicesRun.then(sss => {
 
 // ==========================================================================
 
-=======
-import SubSystems from './subsystems/index.mjs'
-
-import CONFIG from './config.js'
-
-// ==========================================================================
-
-SubSystems.init()
-.then(sss => {
-
-// ==========================================================================
-
-	const search = (req, res, next) => {
-		const userId = mongoose.Types.ObjectId(req.session.user)
-
-		console.log('<search>', req.body)
-
-		const searchString = req.body.q
-
-		const queryArgs = []
-		var query = 'SELECT *' +
-			` FROM ${CONFIG.SphinxQL.indexName}` +
-			' WHERE user = ? AND MATCH(?)'
-
-		queryArgs.push(userId.toString())
-		queryArgs.push(req.body.q)
-
-		if (req.body.kind) {
-			let n = queryArgs.length
-
-			if ((req.body.kind instanceof Array) && req.body.kind.length) {
-				queryArgs.push(req.body.kind)
-			}
-			else
-			if ('string' === typeof req.body.kind) {
-				queryArgs.push(req.body.kind)
-			}
-
-			if (queryArgs.length !== n) {
-				query += ' AND kind IN (?)'
-			}
-		}
-
-		console.log('###', {
-			q: query,
-			a: queryArgs
-		})
-
-		sss.sphinxql.query(
-			query, queryArgs,
-			(err, rows, fields) => {
-
-				if (err) {
-					next(err)
-					return
-				}
-
-				console.log('* Sphinx SELECT rows:', rows.length)
-
-				sss.sphinxql.query(
-					'SHOW META LIKE ?',
-					[ 'total_found' ],
-					(err, meta, fields) => {
-
-						if (err) {
-							console.error(err)
-							return
-						}
-
-						console.log('* Sphinx total_found:', meta[0].Value)
-
-						res.send({
-							total_found: meta[0].Value,
-							result: rows
-						})
-					}
-				)
-			}
-		)
-	}
-
->>>>>>> 655cc14326ff6703898188b64c4af9678b0ffb8e
 // --------------------------------------------------------------------------
 
 	const docEvents = (req, res, next) => {
