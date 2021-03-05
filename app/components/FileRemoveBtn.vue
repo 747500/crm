24293ae9143585b9a88cbee59432d97d6e1bf358
Serge R <747500@gmail.com>
2021-03-05 15:39:21 +0300

    adds FileRemoveBtn vue component

---
+++ app/components/FileRemoveBtn.vue
@@ -0,0 +1,61 @@
+<template lang="pug">
+
+	bsDropdown(:schema="schema")/
+
+</template>
+
+
+<script>
+
+	import bsDropdown from './bs/Dropdown.vue'
+
+	export default {
+
+		name: 'FileRemoveBtn',
+
+		components: {
+			bsDropdown,
+		},
+
+		props: {
+			item: Object,
+		},
+
+		data () {
+			return {
+				schema: {
+					text: '🗑',
+					class: [
+						'btn-sm',
+						//'btn-danger',
+						'btn-outline-danger',
+					],
+					menu: [
+						{
+							text: 'Cancel',
+							handler: this.onCancel
+						},
+						{
+							text: 'Ok',
+							handler: this.onConfirm
+						}
+					]
+				},
+			}
+		},
+
+		methods: {
+			onConfirm() {
+				this.$emit('remove', this.$props.item)
+			},
+
+			onCancel() {
+				// no-op
+				//console.log('onCancel', args)
+			},
+
+		},
+
+	}
+
+</script>
