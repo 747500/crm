44834592935bc406e5cd1fc1b6e5ab35e5533658
Serge R <747500@gmail.com>
2021-03-09 02:59:16 +0300

    renames class to less generic

--- app/components/DocFormOwner.vue
+++ app/components/DocFormOwner.vue
@@ -18,12 +18,12 @@
 					) 🗙
 					button(
 						v-if="false === isOpen"
-						class="open"
+						class="open-btn"
 						@click.prevent="listOpen"
 					) &#709;
 					button(
 						v-if="true === isOpen"
-						class="close"
+						class="close-btn"
 						@click.prevent="listClose"
 					) &#708;
 			// ︿ ﹀
@@ -261,7 +261,7 @@
 	border: none;
 }
 
-.doc-form-owner .select .container .controls button.open,button.close {
+.doc-form-owner .select .container .controls button.open-btn,button.close-btn {
 	transform: scale(2.0,1.0);
 }
 
@@ -270,7 +270,7 @@
 }
 
 /*
-.doc-form-owner .select .container .controls a.close {
+.doc-form-owner .select .container .controls a.close-btn {
 	background-image: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16'%3e%3cpath fill='none' stroke='%23343a40' stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M2 5l6 6 6-6'/%3e%3c/svg%3e");
 	background-repeat: no-repeat;
 	background-position: right 0.75rem center;
