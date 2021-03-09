d9c772a49d56e78188d15c0b131a54405f26af96
Serge R <747500@gmail.com>
2021-03-09 03:01:02 +0300

    use bootstrap styles

--- app/components/List.vue
+++ app/components/List.vue
@@ -1,13 +1,6 @@
 <template lang="pug">
 
-	ul(class="doc-list")
-		li(v-if="$listeners.create" class="create")
-			div(class="btn-group btn-group-sm" role="group" aria-label="")
-				button(@click.prevent="() => $emit('create')" class="btn btn-primary")
-					| +
-
-		// @click.prevent="(event) => switchView(event, p)"
-
+	ul
 		li(v-for="(item, n) in model" :key="item._id")
 			div(
 				v-on="$listeners.open ? { click: () => onClick(item) } : {}"
@@ -15,20 +8,6 @@
 			)
 				slot(v-bind:item="item")
 					pre {{ n }}: {{ item }}
-
-			div(class="toolbar")
-				div(class="btn-group-vertical btn-group-sm" role="group" aria-label="")
-					button(
-						v-if="$listeners.edit"
-						@click.prevent="() => $emit('edit', item)"
-						class="btn btn-secondary"
-					) 🖉
-
-					button(
-						v-if="$listeners.remove"
-						@click.prevent="() => $emit('remove', item)"
-						class="btn btn-danger"
-					) 🗑
 
 </template>
 
@@ -71,38 +50,5 @@ export default {
 </script>
 
 <style>
-
-ul.doc-list {
-	margin: 0 auto;
-	max-width: 50rem;
-	padding: 0.33em 0;
-}
-
-ul.doc-list > li {
-	padding: .33em;
-	display: flex;
-	border-bottom: 1px solid var(--border-color);
-}
-
-ul.doc-list > li:hover {
-	background-color: var(--hover-color);
-}
-
-ul.doc-list > li .item {
-	flex: 10;
-}
-
-ul.doc-list > li .item.active {
-	cursor: pointer;
-}
-
-ul.doc-list > li .toolbar {
-	flex: 1;
-	text-align: right;
-}
-
-ul.doc-list > li.create {
-	list-style-type: circle;
-}
 
 </style>
