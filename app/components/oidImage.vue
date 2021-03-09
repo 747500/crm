8964c6a113a7a44e6838ee62be2a6888f7daa3b1
Serge R <747500@gmail.com>
2021-03-09 03:05:02 +0300

    adds placeholder if not an image

--- app/components/oidImage.vue
+++ app/components/oidImage.vue
@@ -1,10 +1,17 @@
 <template lang="pug">
 
 	iimg(
+		v-if="isImage"
 		:src="src"
 		:alt="alt"
 		:key="key"
 	)/
+	svg(
+		v-else
+		viewBox="0 0 300 300"
+		preserveAspectRatio="xMidYMid meet"
+	)
+		text(x="18" y="220" style="font-size: 90px;") {{ type }}
 
 </template>
 
@@ -25,7 +32,8 @@
 			return {
 				src: null,
 				alt: '',
-				key: ''
+				key: '',
+				type: ''
 			}
 		},
 
@@ -55,6 +63,14 @@
 
 				const caption = response.headers.get('x-meta-caption') || ''
 				this.alt = decodeURIComponent(caption)
+
+				const contentType = response.headers.get('content-type') || 'application/binary'
+				var content
+				[ content, this.type ] = contentType.split('/')
+
+				if ('image' === content) {
+					this.isImage = true
+				}
 
 				this.key = this.$props.oid
 			})
