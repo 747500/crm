9e51e966ef177172f4eb9ac1aeaf4d8ae254ef58
Serge R <747500@gmail.com>
2021-03-10 15:37:35 +0300

    adds lost reply

---
+++ app/components/DocCard.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+
+	bsCard
+		bsCardHeader(v-if="$slots.header")
+			slot(name="header")
+
+		bsCardImage(v-if="$slots.image")
+			slot(name="image")
+
+		bsCardBody(v-if="$slots.header")
+			slot(name="body")
+
+</template>
+
+<script>
+
+export default {
+
+	name: 'DocCard',
+
+}
+
+</script>
