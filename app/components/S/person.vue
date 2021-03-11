2dba8d698d4a7526721751d09f0ccb086c83e5ec
Serge R <747500@gmail.com>
2021-03-11 10:16:05 +0300

    fixes debug output

--- app/components/S/person.vue
+++ app/components/S/person.vue
@@ -39,7 +39,7 @@
 
 		mounted () {
 
-			//console.log('<M/person.vue> mounted', this.$props)
+			//console.log('<S/person.vue> mounted', this.$props)
 
 			if ('string' === typeof this.$props.model) {
 
